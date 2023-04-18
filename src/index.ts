d7e9962e7d1b82733fa4458246664b269bb0e36f
paulSanchezMarmelab <paul@marmelab.com>
2023-04-18 09:28:09 +0200

    [WIP] first implementation of commander parameters for filepath

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,20 @@
 const fs = require('fs');
+import { Command } from 'commander';
 import chalk from "chalk";
 import { drawBoard } from './board/board';
+
+const program = new Command();
+program
+.option('-f, --filepath <string>', "A path to load a kamon game from a file")
+.parse(process.argv);
+
+const programParameters = program.opts();
+console.log(programParameters);
 
 let data = [
     [undefined, {color: 1, symbol: 'C'}, {color: 2, symbol: 'A'}],
     [{color: 3, symbol: 'A'}, {color: 3, symbol: 'C'}, {color: 2, symbol: 'B'}],
     [{color: 2, symbol: 'C'}, {color: 1, symbol: 'A'}, undefined]
 ];
-
-/*let isFileOk = true;
-
-if(typeof process.argv !== "object" || process.argv['f'] == null){
-    console.log(chalk.red('Please use "-f" parameter to set path to config file'));
-    console.log(chalk.yellow('================================'));
-    console.log(chalk.yellow('loading test data instead'));
-}
-else{
-    try {
-        const dataFromFile = fs.readFileSync(process.argv['f'], 'utf8');
-        if(dataFromFile != null){
-            try{
-                data = JSON.parse(dataFromFile)
-            }
-            catch (error) {
-                console.log(chalk.red('Cannnot parse JSON; please check format in config file'));
-                isFileOk = false;
-            }
-            
-        }
-        else{
-            console.log(chalk.red('No data in file'));
-            isFileOk = false;
-            
-        }
-        console.log(data);
-      } catch (err) {
-        console.error(err);
-        isFileOk = false;
-      }
-
-    if(isFileOk === false){
-        console.log(chalk.yellow('================================'));
-        console.log(chalk.yellow('loading test data instead'));
-    }
-}*/
 
 drawBoard(data);
