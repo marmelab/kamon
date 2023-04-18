c5078f0c76772d4245873ad8cb48938dec051d02
adguernier <adrien@marmelab.com>
2023-04-18 10:28:29 +0200

    FIX: rename data in gameConfig

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 const fs = require('fs');
 import { drawBoard } from './board/board';
 
-let data = [
+const gameConfig = [
     [undefined, {color: 1, symbol: 'C'}, {color: 2, symbol: 'A'}],
     [{color: 3, symbol: 'A'}, {color: 3, symbol: 'C'}, {color: 2, symbol: 'B'}],
     [{color: 2, symbol: 'C'}, {color: 1, symbol: 'A'}, undefined]
 ];
 
-drawBoard(data);
+drawBoard(gameConfig);
