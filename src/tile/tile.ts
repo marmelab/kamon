ac4e23bf8a382fc323d78711196b0fb19789c224
paulSanchezMarmelab <paul@marmelab.com>
2023-04-18 16:50:10 +0200

    first implementation of feature

--- src/tile/tile.ts
+++ src/tile/tile.ts
@@ -2,13 +2,32 @@ import chalk from "chalk";
 
 type Colors = 'yellow' | 'blue' | 'red' | 'green' | 'cyan' | 'white'
 type Symbols = 'A' | 'B' | 'C' | 'D' | 'E' | 'F'
+type Players = "black" | "white"
 
 export interface Tile {
     color: Colors,
-    symbol: Symbols
+    symbol: Symbols,
+    playedBy: Players,
+    lastPlayed: boolean
 }
 
 export const generateCliText = (tile: Tile): string => {
-    const dynamicChalk = chalk[tile.color];
-    return dynamicChalk(tile.symbol)
+    let dynamicChalk = chalk[tile.color];
+
+    if(tile.playedBy != null){
+        if(tile.playedBy === "black"){
+            dynamicChalk = dynamicChalk.bgWhite.dim
+        }
+
+        if(tile.playedBy === "white"){
+            dynamicChalk = dynamicChalk.bgWhite
+        }
+        
+    }
+
+    if(tile.lastPlayed != null){
+        dynamicChalk = dynamicChalk.bold.underline
+    }
+    
+    return dynamicChalk(tile.symbol);
 }
