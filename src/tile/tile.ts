18d814c90b14bd1c019b97420182c9afa7885121
adguernier <adrien@marmelab.com>
2023-04-18 11:46:10 +0200

    FIX: Color and Symbol type without s

--- src/tile/tile.ts
+++ src/tile/tile.ts
@@ -1,11 +1,11 @@
 import chalk from "chalk";
 
-type Colors = 'yellow' | 'blue' | 'red' | 'green' | 'cyan' | 'white'
-type Symbols = 'A' | 'B' | 'C' | 'D' | 'E' | 'F'
+type Color = 'yellow' | 'blue' | 'red' | 'green' | 'cyan' | 'white'
+type Symbol = 'A' | 'B' | 'C' | 'D' | 'E' | 'F'
 
 export interface Tile {
-    color: Colors,
-    symbol: Symbols
+    color: Color,
+    symbol: Symbol
 }
 
 export const renderTile = (tile: Tile): string => {
