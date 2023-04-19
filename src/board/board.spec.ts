6526bb3b724cb4a0d14522b5c374ac8e5bd05be4
paulSanchezMarmelab <paul@marmelab.com>
2023-04-19 09:47:47 +0200

    added assertion to renderBoard test

--- src/board/board.spec.ts
+++ src/board/board.spec.ts
@@ -14,6 +14,6 @@ describe("renderBoard", () => {
         const board: Board = [[undefined, {"color": "red", "symbol": "A"}, {"color": "blue", "symbol": "A"}],
         [{"color": "red", "symbol": "A"}, {"color": "blue", "symbol": "C"}, {"color": "red", "symbol": "B"}],
         [{"color": "blue", "symbol": "C"}, {"color": "yellow", "symbol": "A"}, undefined]];
-        expect(renderBoard(board));
+        expect(renderBoard(board)).not.toThrow();
     });
 });
