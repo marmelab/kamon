893057491a761d072fa272be01d734d4292df9eb
paulSanchezMarmelab <paul@marmelab.com>
2023-04-19 10:03:59 +0200

    fixed bad "it" description text

--- src/board/board.spec.ts
+++ src/board/board.spec.ts
@@ -1,19 +1,33 @@
 import { Tile } from "../tile/tile";
-import {renderLine, Board, renderBoard, NullableTile} from "./board"
+import { renderLine, Board, renderBoard, NullableTile } from "./board";
 
 describe("renderLine", () => {
-    it("Testing a line with both undefined and Tile values", () => {
-        const tile: Tile = {color: "blue", symbol: "A"} ;
-        const line: NullableTile[] = [undefined, tile, undefined];
-        expect(renderLine(line)).toMatch(/\s.*A.*\s/);
-    });
+  it("should render a line with blank characters", () => {
+    const tile: Tile = { color: "blue", symbol: "A" };
+    const line: NullableTile[] = [undefined, tile, undefined];
+    expect(renderLine(line)).toMatch(/\s.*A.*\s/);
+  });
 });
 
 describe("renderBoard", () => {
-    it("executing renderBoard with mock data", () => {
-        const board: Board = [[undefined, {"color": "red", "symbol": "A"}, {"color": "blue", "symbol": "A"}],
-        [{"color": "red", "symbol": "A"}, {"color": "blue", "symbol": "C"}, {"color": "red", "symbol": "B"}],
-        [{"color": "blue", "symbol": "C"}, {"color": "yellow", "symbol": "A"}, undefined]];
-        expect(renderBoard(board)).not.toThrow();
-    });
+  it("should render a board", () => {
+    const board: Board = [
+      [
+        undefined,
+        { color: "red", symbol: "A" },
+        { color: "blue", symbol: "A" },
+      ],
+      [
+        { color: "red", symbol: "A" },
+        { color: "blue", symbol: "C" },
+        { color: "red", symbol: "B" },
+      ],
+      [
+        { color: "blue", symbol: "C" },
+        { color: "yellow", symbol: "A" },
+        undefined,
+      ],
+    ];
+    expect(renderBoard(board)).not.toThrow();
+  });
 });
--- src/tile/tile.spec.ts
+++ src/tile/tile.spec.ts
@@ -1,9 +1,9 @@
 import expect from "expect";
-import {Tile, renderTile} from "./tile";
+import { Tile, renderTile } from "./tile";
 
 describe("renderTile", () => {
-    it("Testing with Tile input", () => {
-        const tile: Tile = {color: "blue", symbol: "A"};
-        expect(renderTile(tile)).toMatch(/A/);
-    });
+  it("should render a tile", () => {
+    const tile: Tile = { color: "blue", symbol: "A" };
+    expect(renderTile(tile)).toMatch(/A/);
+  });
 });
