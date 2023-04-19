2b522017ea821f5da2bb632217100b8dc08fcd20
adguernier <adrien@marmelab.com>
2023-04-19 10:05:50 +0200

    FIX: remove useless import

--- src/board/board.ts
+++ src/board/board.ts
@@ -1,25 +1,25 @@
 import chalk from "chalk";
-import {renderTile, PlayableTile, Tile} from "../tile/tile";
+import { renderTile, Tile } from "../tile/tile";
 
-export type Board = Tile[][]
+export type Board = Tile[][];
 
-const BLANK_CHAR = chalk.black(' ')
+const BLANK_CHAR = chalk.black(" ");
 
 const renderLine = (lines: Tile[]): string => {
-    const line = ''        
-    return lines.reduce((accumulator, tile) => {
-        if (tile == undefined) {
-            return accumulator + BLANK_CHAR
-        }
-        
-        return accumulator + renderTile(tile)
-    }, line)
-}
+  const line = "";
+  return lines.reduce((accumulator, tile) => {
+    if (tile == undefined) {
+      return accumulator + BLANK_CHAR;
+    }
+
+    return accumulator + renderTile(tile);
+  }, line);
+};
 
 export const renderBoard = (data: Board) => {
-    console.log(chalk.white.bold('-------------'))
-    data.forEach(lines => {
-        console.log(renderLine(lines))
-    })
-    console.log(chalk.white.bold('-------------'))
-}   
+  console.log(chalk.white.bold("-------------"));
+  data.forEach((lines) => {
+    console.log(renderLine(lines));
+  });
+  console.log(chalk.white.bold("-------------"));
+};
