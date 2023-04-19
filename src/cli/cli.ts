fa0c7e1d8fc82e027e4f0f0dd189c226ef12024a
paulSanchezMarmelab <paul@marmelab.com>
2023-04-19 09:53:46 +0200

    switech to path.join to avoid security issues

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -1,19 +1,23 @@
-import { Command } from 'commander';
+import { Command } from "commander";
+import * as path from "path";
 
-let program : Command;
+let program: Command;
 
 export const initCLI = () => {
-    program = new Command();
-    program
-    .option('-f, --filepath <string>', "A path to load a kamon game from a file")
+  program = new Command();
+  program
+    .option(
+      "-f, --filepath <string>",
+      "A path to load a kamon game from a file"
+    )
     .parse(process.argv);
-}
+};
 
-export const getFilePath = ():string => {
-    const filePathParameter = program.opts()['filepath'];
+export const getFilePath = (): string => {
+  const filePathParameter = program.opts()["filepath"];
 
-    return filePathParameter ?? `${__dirname}/../../save.json`
-}
-
-
-
+  return (
+    filePathParameter ??
+    path.normalize(path.join(`${__dirname}`, "..", "..", "save.json"))
+  );
+};
