97e8a81d69a47b5891095cb12107613e4cb72f63
adguernier <adrien@marmelab.com>
2023-04-19 11:47:15 +0200

    remove unused import

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { Board, renderBoard } from "./board/board";
 import { initCLI } from "./cli";
 import { loadGameConfigFromFile } from "./gameLoader";
-import { initRandomGame } from "./randomizer/randomizer";
 
 initCLI();
 const gameConfig: Board = loadGameConfigFromFile();
