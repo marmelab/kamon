5f63f8683a4567ef395197e4c2d99d277ebcc97c
paulSanchezMarmelab <paul@marmelab.com>
2023-04-20 11:57:15 +0200

    fixed tests and added a new one

--- src/move/move.spec.ts
+++ src/move/move.spec.ts
@@ -2,10 +2,14 @@ import { checkIfMoveIsAllowed } from "./move";
 
 describe("checkIfMoveIsAllowed", () => {
   it("should block move on unallowed first move tile", () => {
-    expect(checkIfMoveIsAllowed({ x: 1, y: 3 }, true)).toBeFalsy();
+    expect(checkIfMoveIsAllowed({ x: 3, y: 0 }, true)).toBeFalsy();
   });
 
   it("should allow move on allowed first move tile", () => {
-    expect(checkIfMoveIsAllowed({ x: 1, y: 3 }, false)).toBeTruthy();
+    expect(checkIfMoveIsAllowed({ x: 4, y: 0 }, true)).toBeTruthy();
+  });
+
+  it("should allow any move after first turn", () => {
+    expect(checkIfMoveIsAllowed({ x: 3, y: 0 }, false)).toBeTruthy();
   });
 });
