12ef8f18cc4cd526d0d89ddb4f0d42669bc104db
paulSanchezMarmelab <paul@marmelab.com>
2023-04-20 15:46:15 +0200

    added top separator back

--- src/board/board.ts
+++ src/board/board.ts
@@ -28,6 +28,7 @@ export const renderLine = (lines: NullableTile[]): string => {
 };
 
 export const renderBoard = (data: Board) => {
+  console.log(chalk.white.bold("-------------"));
   data.forEach((lines) => {
     console.log(renderLine(lines));
   });
