dc0bd4a913a23e76acfc82759fe8a9db2a6af2a8
adguernier <adrien@marmelab.com>
2023-04-20 11:46:23 +0200

    FIX: check NEUTRALE_TILE.symbol instaed of "O"

--- src/prompt/choices.ts
+++ src/prompt/choices.ts
@@ -1,8 +1,8 @@
-import { Tile, flatternTiles } from "../tile/tile";
+import { NEUTRALE_TILE, Tile, flatternTiles } from "../tile/tile";
 
 export const generateChoices = (gameConfig) =>
   flatternTiles(gameConfig).reduce((acc, tile: Tile) => {
-    if (tile != undefined && tile.symbol !== "O") {
+    if (tile != undefined && tile.symbol !== NEUTRALE_TILE.symbol) {
       acc.push({
         title: `${tile.color} ${tile.symbol}`,
         value: tile,
