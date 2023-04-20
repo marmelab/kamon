74884fd2b290a273fb9cfa78b5ff08e2d81c8f22
paulSanchezMarmelab <paul@marmelab.com>
2023-04-20 15:52:36 +0200

    removed unused property of renderline

--- src/board/board.spec.ts
+++ src/board/board.spec.ts
@@ -5,7 +5,7 @@ describe("renderLine", () => {
   it("should render a line with blank characters", () => {
     const tile: Tile = { color: "blue", symbol: "A" };
     const line: NullableTile[] = [undefined, tile, undefined];
-    expect(renderLine(line, 0)).toMatch(/\s.*A.*\s/);
+    expect(renderLine(line)).toMatch(/\s.*A.*\s/);
   });
 });
 
