eca6bc562d20f9aa493910e3fc8d69a920539ea8
adguernier <adrien@marmelab.com>
2023-04-21 13:17:21 +0200

    toISOString instead of join, etc

--- src/game/save.ts
+++ src/game/save.ts
@@ -2,13 +2,7 @@ import * as fs from "fs";
 import { Board } from "../board/board";
 
 export const save = (board: Board) => {
-  const date = new Date()
-    .toUTCString()
-    .replace(",", "")
-    .split(" ")
-    .join("-")
-    .split(":")
-    .join("-");
+  const date = new Date().toISOString();
   const fileName = `kamon-saved-${date}.json`;
   fs.writeFileSync(fileName, JSON.stringify(board));
   console.log(`Saved file: ${fileName}`);
