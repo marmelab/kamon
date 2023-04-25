08318ed2b9b4b8919dca0b7642edb27b3e6a0288
adguernier <adrien@marmelab.com>
2023-04-25 12:35:37 +0200

    make e2e test work

--- webapp/test/app.e2e-spec.ts
+++ webapp/test/app.e2e-spec.ts
@@ -1,10 +1,12 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { Test, TestingModule } from "@nestjs/testing";
+import { INestApplication } from "@nestjs/common";
+import * as request from "supertest";
+import { AppModule } from "./../src/app.module";
+import { join } from "path";
+import { NestExpressApplication } from "@nestjs/platform-express";
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+describe("AppController (e2e)", () => {
+  let app: NestExpressApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -12,13 +14,13 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useStaticAssets(join(__dirname, "..", "public"));
+    app.setBaseViewsDir(join(__dirname, "..", "views"));
+    app.setViewEngine("hbs");
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it("/ (GET)", () => {
+    return request(app.getHttpServer()).get("/").expect(200);
   });
 });
