a7499259e9dd04c0711c328d8549e3c49557cef1
paulSanchezMarmelab <paul@marmelab.com>
2023-04-25 14:16:25 +0200

    added partials; added static gamestate to main

--- webapp/src/app.controller.ts
+++ webapp/src/app.controller.ts
@@ -4,213 +4,223 @@ export class AppController {
   @Get()
   @Render("index")
   root() {
-    const game = [
-      [
-        null,
-        null,
-        null,
-        {
-          symbol: "B",
-          color: "cyan",
-        },
-        {
-          symbol: "A",
-          color: "green",
-        },
-        {
-          symbol: "D",
-          color: "cyan",
-        },
-        {
-          symbol: "A",
-          color: "red",
-        },
-        null,
-        null,
-        null,
+    const game = {
+      gameState: {
+        currentPlayer: "black",
+        isRunning: true,
+        winner: "white",
+        isDraw: false,
+        turnNumber: 2,
+        message: "Welcome to Kamon 🍱 ! Black player, you turn",
+      },
+      board: [
+        [
+          null,
+          null,
+          null,
+          {
+            symbol: "B",
+            color: "cyan",
+          },
+          {
+            symbol: "A",
+            color: "green",
+          },
+          {
+            symbol: "D",
+            color: "cyan",
+          },
+          {
+            symbol: "A",
+            color: "red",
+          },
+          null,
+          null,
+          null,
+        ],
+        [
+          null,
+          null,
+          {
+            symbol: "E",
+            color: "magenta",
+            playedBy: "white",
+          },
+          {
+            symbol: "F",
+            color: "blue",
+            playedBy: "white",
+          },
+          {
+            symbol: "C",
+            color: "green",
+            playedBy: "white",
+          },
+          {
+            symbol: "E",
+            color: "green",
+            playedBy: "black",
+            lastPlayed: false,
+          },
+          {
+            symbol: "D",
+            color: "green",
+            playedBy: "black",
+            lastPlayed: true,
+          },
+          null,
+          null,
+        ],
+        [
+          null,
+          {
+            symbol: "C",
+            color: "cyan",
+          },
+          {
+            symbol: "D",
+            color: "blue",
+          },
+          {
+            symbol: "C",
+            color: "yellow",
+          },
+          {
+            symbol: "B",
+            color: "yellow",
+            playedBy: "black",
+            lastPlayed: false,
+          },
+          {
+            symbol: "B",
+            color: "blue",
+          },
+          {
+            symbol: "A",
+            color: "cyan",
+          },
+          null,
+        ],
+        [
+          {
+            symbol: "D",
+            color: "red",
+          },
+          {
+            symbol: "F",
+            color: "cyan",
+          },
+          {
+            symbol: "B",
+            color: "red",
+          },
+          {
+            symbol: "C",
+            color: "magenta",
+            playedBy: "black",
+            lastPlayed: false,
+          },
+          {
+            symbol: "E",
+            color: "yellow",
+          },
+          {
+            symbol: "O",
+            color: "grey",
+            playedBy: null,
+            lastPlayed: false,
+          },
+          {
+            symbol: "A",
+            color: "yellow",
+          },
+        ],
+        [
+          null,
+          {
+            symbol: "F",
+            color: "yellow",
+          },
+          {
+            symbol: "E",
+            color: "red",
+          },
+          {
+            symbol: "E",
+            color: "blue",
+            playedBy: "black",
+            lastPlayed: false,
+          },
+          {
+            symbol: "F",
+            color: "green",
+          },
+          {
+            symbol: "D",
+            color: "yellow",
+          },
+          {
+            symbol: "F",
+            color: "magenta",
+          },
+          null,
+        ],
+        [
+          null,
+          null,
+          {
+            symbol: "A",
+            color: "blue",
+            playedBy: "black",
+            lastPlayed: false,
+          },
+          {
+            symbol: "D",
+            color: "magenta",
+            playedBy: "black",
+            lastPlayed: false,
+          },
+          {
+            symbol: "A",
+            color: "magenta",
+          },
+          {
+            symbol: "B",
+            color: "magenta",
+          },
+          {
+            symbol: "B",
+            color: "green",
+          },
+          null,
+          null,
+        ],
+        [
+          null,
+          null,
+          null,
+          {
+            symbol: "C",
+            color: "blue",
+          },
+          {
+            symbol: "E",
+            color: "cyan",
+          },
+          {
+            symbol: "F",
+            color: "red",
+          },
+          {
+            symbol: "C",
+            color: "red",
+          },
+          null,
+          null,
+          null,
+        ],
       ],
-      [
-        null,
-        null,
-        {
-          symbol: "E",
-          color: "magenta",
-          playedBy: "white",
-        },
-        {
-          symbol: "F",
-          color: "blue",
-          playedBy: "white",
-        },
-        {
-          symbol: "C",
-          color: "green",
-          playedBy: "white",
-        },
-        {
-          symbol: "E",
-          color: "green",
-          playedBy: "black",
-          lastPlayed: false,
-        },
-        {
-          symbol: "D",
-          color: "green",
-          playedBy: "black",
-          lastPlayed: true,
-        },
-        null,
-        null,
-      ],
-      [
-        null,
-        {
-          symbol: "C",
-          color: "cyan",
-        },
-        {
-          symbol: "D",
-          color: "blue",
-        },
-        {
-          symbol: "C",
-          color: "yellow",
-        },
-        {
-          symbol: "B",
-          color: "yellow",
-          playedBy: "black",
-          lastPlayed: false,
-        },
-        {
-          symbol: "B",
-          color: "blue",
-        },
-        {
-          symbol: "A",
-          color: "cyan",
-        },
-        null,
-      ],
-      [
-        {
-          symbol: "D",
-          color: "red",
-        },
-        {
-          symbol: "F",
-          color: "cyan",
-        },
-        {
-          symbol: "B",
-          color: "red",
-        },
-        {
-          symbol: "C",
-          color: "magenta",
-          playedBy: "black",
-          lastPlayed: false,
-        },
-        {
-          symbol: "E",
-          color: "yellow",
-        },
-        {
-          symbol: "O",
-          color: "grey",
-          playedBy: null,
-          lastPlayed: false,
-        },
-        {
-          symbol: "A",
-          color: "yellow",
-        },
-      ],
-      [
-        null,
-        {
-          symbol: "F",
-          color: "yellow",
-        },
-        {
-          symbol: "E",
-          color: "red",
-        },
-        {
-          symbol: "E",
-          color: "blue",
-          playedBy: "black",
-          lastPlayed: false,
-        },
-        {
-          symbol: "F",
-          color: "green",
-        },
-        {
-          symbol: "D",
-          color: "yellow",
-        },
-        {
-          symbol: "F",
-          color: "magenta",
-        },
-        null,
-      ],
-      [
-        null,
-        null,
-        {
-          symbol: "A",
-          color: "blue",
-          playedBy: "black",
-          lastPlayed: false,
-        },
-        {
-          symbol: "D",
-          color: "magenta",
-          playedBy: "black",
-          lastPlayed: false,
-        },
-        {
-          symbol: "A",
-          color: "magenta",
-        },
-        {
-          symbol: "B",
-          color: "magenta",
-        },
-        {
-          symbol: "B",
-          color: "green",
-        },
-        null,
-        null,
-      ],
-      [
-        null,
-        null,
-        null,
-        {
-          symbol: "C",
-          color: "blue",
-        },
-        {
-          symbol: "E",
-          color: "cyan",
-        },
-        {
-          symbol: "F",
-          color: "red",
-        },
-        {
-          symbol: "C",
-          color: "red",
-        },
-        null,
-        null,
-        null,
-      ],
-    ];
+    };
     return {
       game,
     };
