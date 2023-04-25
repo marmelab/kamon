c4a0350d4162660a772ef6a77d0c35c599a0a8da
adguernier <adrien@marmelab.com>
2023-04-25 15:57:06 +0200

    the board is now a form with board state

--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -2,6 +2,19 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import { join } from "path";
+import helpers from "handlebars-helpers";
+
+import { handlebars } from "hbs";
+
+handlebars.registerHelper("isdefined", function (value) {
+  return value !== undefined;
+});
+
+handlebars.registerHelper("isundefined", function (value) {
+  return value == undefined;
+});
+
+helpers({ handlebars });
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
