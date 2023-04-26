e06f2fdc6199ba5661ea3edbdb332452baafbbba
adguernier <adrien@marmelab.com>
2023-04-26 11:51:27 +0200

    FIX: helpers is not a function

--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import { join } from "path";
-import helpers from "handlebars-helpers";
+import * as helpers from "handlebars-helpers";
 import { handlebars } from "hbs";
 import { registerPartials } from "./partials";
 
