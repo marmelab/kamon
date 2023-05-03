1354ef88885b1c1d6a1f036e77076eae3a75750a
adguernier <adrien@marmelab.com>
2023-05-03 15:43:53 +0200

    split components in separate files

--- mobileapp/App.js
+++ mobileapp/App.js
@@ -1,22 +1,21 @@
-import List from "components/home/HomeComponent";
+import { Home } from "components/home/Home";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <List />
-      <Text>Hello world !</Text>
+    <SafeAreaView style={styles.container}>
+      <Home />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: StatusBar.currentHeight || 0,
     flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "space-between",
+    backgroundColor: "#112b3c",
     padding: 10,
   },
 });
