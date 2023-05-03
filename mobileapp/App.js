941fbb8d51a051dff4436bc2d4e8f04c4970d0ff
paulSanchezMarmelab <paul@marmelab.com>
2023-05-03 10:07:27 +0200

    added background color

--- mobileapp/App.js
+++ mobileapp/App.js
@@ -217,7 +217,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#112b3c",
     alignItems: "center",
     justifyContent: "center",
   },
