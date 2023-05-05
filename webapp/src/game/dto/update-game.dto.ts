64daf7142c7173f83ef7ea8160bf0a7e164e89c7
adguernier <adrien@marmelab.com>
2023-05-05 18:21:02 +0200

    remove useless comment

--- webapp/src/game/dto/update-game.dto.ts
+++ webapp/src/game/dto/update-game.dto.ts
@@ -2,44 +2,6 @@ import { PartialType } from "@nestjs/mapped-types";
 import { CreateGameDto } from "./create-game.dto";
 import { ApiProperty } from "@nestjs/swagger";
 export class UpdateGameDto extends PartialType(CreateGameDto) {
-  /*@ApiProperty({
-    description: "The board with all tiles",
-    type: "array",
-    items: {
-      type: "array",
-      description: "The tiles",
-      items: {
-        type: "object",
-        properties: {
-          color: {
-            type: "string",
-            example: "yellow",
-          },
-          symbol: {
-            type: "string",
-            example: "A",
-          },
-          moveAllowed: {
-            type: "boolean",
-          },
-          playedBy: {
-            type: "string",
-            example: "black",
-          },
-          lastPlayed: {
-            type: "boolean",
-          },
-        },
-      },
-    },
-  })
-  board: Board;
-
-  @ApiProperty({
-    description: "The state of the game",
-  })
-  gameState: GameState;*/
-
   @ApiProperty({
     description: "The coordinates (x-y) of the tile that has been played",
     type: "string",
