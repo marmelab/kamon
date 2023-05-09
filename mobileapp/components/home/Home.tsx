8d508eb6df22bef7457fd8fe7aba544bb934377d
adguernier <adrien@marmelab.com>
2023-05-09 21:25:14 +0200

    add button to create a new game

--- mobileapp/components/home/Home.tsx
+++ mobileapp/components/home/Home.tsx
@@ -1,9 +1,26 @@
-import { StyleSheet, View } from "react-native";
+import { Button, StyleSheet, View } from "react-native";
 import { GameList } from "./GameList";
+import { API_ENDPOINT } from "@env";
 
 export const Home = ({ navigation }) => {
+  const createGame = () => {
+    const url = new URL("game/create", API_ENDPOINT);
+    fetch(url, {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((r) => r.json())
+      .then((game) =>
+        navigation.navigate("Game", { itemId: game.id, playable: true }),
+      );
+  };
+
   return (
     <View style={styles.container}>
+      <Button title="Play online" onPress={createGame} />
       <GameList navigation={navigation} />
     </View>
   );
