87d178375faabf9efc120600e9daa520176f5385
adguernier <adrien@marmelab.com>
2023-05-09 15:23:19 +0200

    use env variables to store app secret

--- webapp/src/auth/auth.module.ts
+++ webapp/src/auth/auth.module.ts
@@ -3,19 +3,24 @@ import { AuthService } from "./auth.service";
 import { UsersModule } from "../users/users.module";
 import { JwtModule } from "@nestjs/jwt";
 import { AuthController } from "./auth.controller";
-import { jwtConstants } from "./constants";
 import { PassportModule } from "@nestjs/passport";
 import { LocalStrategy } from "./local.strategy";
 import { SessionSerializer } from "./session.serializer";
 import { JwtStrategy } from "./jwt.strategy";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
   imports: [
+    ConfigModule,
     UsersModule,
-    JwtModule.register({
-      global: true,
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: "1h" },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>("SECRET"),
+        signOptions: { expiresIn: "1h" },
+        global: true,
+      }),
+      inject: [ConfigService],
     }),
     PassportModule.register({ session: true }),
   ],
--- webapp/src/auth/constants.ts
+++
@@ -1,4 +0,0 @@
-export const jwtConstants = {
-  //secret: process.env.SECRET,
-  secret: "unsecretbiengarde",
-};
