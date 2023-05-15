e242a792fa3efbececba08076b429d52554e0437
adguernier <adrien@marmelab.com>
2023-05-15 16:18:52 +0200

    use @raphiniert/ra-data-postgrest provider instead a custom provider

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
 import { Admin, Resource } from "react-admin";
-import { dataProvider } from "./dataProvider";
 import { UserList } from "./users";
+import postgrestRestProvider from "@raphiniert/ra-data-postgrest";
 
 const App = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin dataProvider={postgrestRestProvider("http://localhost:3001")}>
     <Resource name="user" list={UserList} />
   </Admin>
 );
