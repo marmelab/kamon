626240f336928c6fa4f794021096ad5fd75684b3
adguernier <adrien@marmelab.com>
2023-05-15 16:18:19 +0200

    delete id field

--- admin/src/users.tsx
+++ admin/src/users.tsx
@@ -3,7 +3,6 @@ import { Datagrid, List, TextField } from "react-admin";
 export const UserList = () => (
   <List>
     <Datagrid rowClick="edit">
-      <TextField source="id" />
       <TextField source="username" />
       <TextField source="email" />
       <TextField source="createdAt" />
