6a9e6c0cb15f08c34966cb1b2d8b2c558bd8d7c5
adguernier <adrien@marmelab.com>
2023-05-15 15:30:05 +0200

    show user list in React Admin

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import { Admin, Resource, ListGuesser } from "react-admin";
-import jsonServerProvider from "ra-data-json-server";
-
-const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
+import { dataProvider } from "./dataProvider";
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="users" list={ListGuesser} />
+    <Resource name="user" list={ListGuesser} />
   </Admin>
 );
 
