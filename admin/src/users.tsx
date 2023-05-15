0ec7c2e3163fb93e436e50ddc1a14991ff725890
adguernier <adrien@marmelab.com>
2023-05-15 17:50:38 +0200

    filter user by email and username

--- admin/src/users.tsx
+++ admin/src/users.tsx
@@ -1,7 +1,12 @@
-import { Datagrid, List, TextField } from "react-admin";
+import { Datagrid, List, TextField, TextInput } from "react-admin";
+
+const userFilters = [
+  <TextInput source="username@ilike" label="Username" alwaysOn />,
+  <TextInput label="Email" source="email@ilike" alwaysOn />,
+];
 
 export const UserList = () => (
-  <List>
+  <List filters={userFilters}>
     <Datagrid rowClick="edit">
       <TextField source="username" />
       <TextField source="email" />
