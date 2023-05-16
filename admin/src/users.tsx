a03fae66eb7065ed4215c747694f619cb629414e
adguernier <adrien@marmelab.com>
2023-05-16 10:51:15 +0200

    remove field user email

--- admin/src/users.tsx
+++ admin/src/users.tsx
@@ -2,14 +2,12 @@ import { Datagrid, List, TextField, TextInput } from "react-admin";
 
 const userFilters = [
   <TextInput source="username@ilike" label="Username" alwaysOn />,
-  <TextInput label="Email" source="email@ilike" alwaysOn />,
 ];
 
 export const UserList = () => (
   <List filters={userFilters}>
     <Datagrid rowClick="edit">
       <TextField source="username" />
-      <TextField source="email" />
       <TextField source="createdAt" />
       <TextField source="updatedAt" />
     </Datagrid>
