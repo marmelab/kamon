c17aaa7f4c718345569a08c4ff8f20375bd91351
adguernier <adrien@marmelab.com>
2023-05-16 16:38:31 +0200

    test GameVictoryField

--- admin/src/ui/gameVictoryField.spec.tsx
+++ admin/src/ui/gameVictoryField.spec.tsx
@@ -11,3 +11,39 @@ test("<GameVictoryField> correctly renders draw", async () => {
   );
   await screen.findByText("Draw");
 });
+
+test("<GameVictoryField> correctly renders loop", async () => {
+  render(
+    <AdminContext>
+      <GameVictoryField record={{ gameState: { isLoop: true } }} />
+    </AdminContext>,
+  );
+  await screen.findByText("Loop");
+});
+
+test("<GameVictoryField> correctly renders path", async () => {
+  render(
+    <AdminContext>
+      <GameVictoryField record={{ gameState: { isPath: true } }} />
+    </AdminContext>,
+  );
+  await screen.findByText("Path");
+});
+
+test("<GameVictoryField> correctly renders blocked", async () => {
+  render(
+    <AdminContext>
+      <GameVictoryField record={{ gameState: { winner: "black" } }} />
+    </AdminContext>,
+  );
+  await screen.findByText("Blocked");
+});
+
+test("<GameVictoryField> correctly renders --", async () => {
+  render(
+    <AdminContext>
+      <GameVictoryField record={{ gameState: {} }} />
+    </AdminContext>,
+  );
+  await screen.findByText("--");
+});
