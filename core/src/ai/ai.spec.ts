d7977a907f9d327cf258a07e619d328c6d6028bb
adguernier <adrien@marmelab.com>
2023-05-19 16:38:21 +0200

    test to find 2 missing tiles for path

--- core/src/ai/ai.spec.ts
+++ core/src/ai/ai.spec.ts
@@ -4,11 +4,22 @@ import { getMockFromJson } from "../mocks/getMock";
 import { getMissingTilesForPath, highlightMissingTilesForPath } from "./ai";
 
 describe("findNextMove", () => {
-  it("should find missing tiles for path", () => {
+  it("should find 1 missing tile for path", () => {
     const { state, board }: { state: GameState; board: Board } =
       getMockFromJson("games/almostPath.json");
     const tiles = getMissingTilesForPath(state.currentPlayer, board);
     expect(tiles).toEqual([{ symbol: "A", color: "yellow" }]);
+  });
+
+  it("should find 2 missing tiles for path", () => {
+    const { state, board }: { state: GameState; board: Board } =
+      getMockFromJson("games/almostPath2.json");
+    const tiles = getMissingTilesForPath(state.currentPlayer, board);
+    console.log(tiles);
+    expect(tiles).toEqual([
+      { symbol: "E", color: "cyan" },
+      { symbol: "E", color: "yellow" },
+    ]);
   });
 
   it("should highlight missing tiles for path", () => {
